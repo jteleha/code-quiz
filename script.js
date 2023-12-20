bf7072e259239b8e8673cc6c87b7c45c84fa62d5
jteleha <telehajoel@gmail.com>
2023-12-19 22:45:01 -0500

    Reworking JS and added in question array

--- script.js
+++ script.js
@@ -1,41 +1,50 @@
-const quizData = [
-    {
-        question: "Which element do we put Javascript in?",
-        choices: ["<script>", "<javascript>", "<scripting>", "<js>"],
-        correctAnswer: "<script>"
-    },
-    {
-        question: "What occurs when the user clicks on an HTML element?",
-        choices: ["onmouseclick", "onchange", "onmouseover", "onclick"],
-        correctAnswer: "onclick"
-    }
-];
+const startButton = document.querySelector("#startButton");
 
-let currentQuestion = 0;
-let score = 0;
-let timeLeft = 10;
+startButton.addEventListener("click, startQuiz");
+
+let time = 0
 let timerInterval;
 
-const startButton = document.getElementById('start-button');
-const submitButton = document.getElementById('submit-button');
-const saveButton = document.getElementById('save-button');
-const initialsInput = document.getElementById("initials");
-
-startButton.addEventListener('click', startQuiz);
-submitButton.addEventListener("click", submitAnswer);
-saveButton.addEventListener("click", saveScore);
-
 function startQuiz() {
-    startButton.style.display = "none";
-    displayQuestion();
     timerInterval = setInterval(updateTimer, 1000);
+    presentQuestion()
 }
 
-function displayQuestion() {
-    const questionElement = document.getElementById("question");
-    const choicesElement = document.getElementById("choices");
-    const currentQuizData = quizData[currentQuizData.question];
-    choicesElement.innerHTML = "";
-
-    currentQuizData.choices
+function updateTimer() {
+    time++;
 }
+
+function presentQuestion() {
+    const firstQuestion = quizQuestions[0];
+    questionElement.textContent = firstQuestion.question;
+
+}
+
+const quizQuestions = [
+    {
+        question: "Commonly used data types do NOT include:",
+        options: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+        answer: "2"
+
+    },
+    {
+        question: "The condition in an if/then statement is enclosed within _____.",
+        options: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
+        answer: "1"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ____.",
+        options: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+        answer: "3"
+    },
+    {
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        options: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
+        answer: "2"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        options: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"],
+        answer: "3"
+    },
+];
