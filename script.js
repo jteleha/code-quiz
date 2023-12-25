e21396168ee94caad090dbc4d6fae909716012f6
jteleha <telehajoel@gmail.com>
2023-12-24 22:17:03 -0500

    CSS Adjustments

--- script.js
+++ script.js
@@ -1,23 +1,81 @@
-const startButton = document.querySelector("#startButton");
+const startButton = document.querySelector(".startButton");
+const saveButton = document.getElementById("save-button");
+const quizContainer = document.getElementById("quiz-container");
 
-startButton.addEventListener("click, startQuiz");
+startButton.addEventListener("click", startQuiz);
+saveButton.addEventListener("click", saveScore);
 
-let time = 0
+let time = 30
 let timerInterval;
+let currentQuestionIndex = 0;
 
 function startQuiz() {
-    timerInterval = setInterval(updateTimer, 1000);
-    presentQuestion()
+    timerInterval = setInterval(updateTimer, 3000);
+    presentQuestion();
 }
 
 function updateTimer() {
-    time++;
+    document.getElementById("timer").textContent = time;
+    time--;
+    if (time < 0) {
+        clearInterval(timerInterval);
+        endQuiz();
+    }
 }
 
 function presentQuestion() {
-    const firstQuestion = quizQuestions[0];
-    questionElement.textContent = firstQuestion.question;
+    const questionElement = document.getElementById("question");
+    const answersElement = document.getElementById("answers");
+    const currentQuestion = quizQuestions[currentQuestionIndex];
+    
+    questionElement.textContent = currentQuestion.question;
+    
+    while (answersElement.firstChild) {
+        answersElement.removeChild(answersElement.firstChild);
+    }
 
+    for (let i = 0; i < currentQuestion.options.length; i++) {
+        const option = currentQuestion.options[i];
+        const answerButton = document.createElement("button");
+        answerButton.textContent = option;
+        answerButton.setAttribute("data-index", i);
+        answerButton.addEventListener("click", handleAnswerClick);
+        answersElement.appendChild(answerButton);
+    }
+
+}
+
+function handleAnswerClick(event) {
+    const selectedOptionIndex = event.target.getAttribute("data-index");
+    checkAnswer(selectedOptionIndex);
+}
+
+function checkAnswer(selectedOptionIndex) {
+    const currentQuestion = quizQuestions[currentQuestionIndex];
+    const correctAnswerIndex = parseInt(currentQuestion.answer);
+
+    if (selectedOptionIndex === correctAnswerIndex.toString()) {
+        incrementScore();
+    } else {
+
+    }
+    currentQuestionIndex++;
+    if (currentQuestionIndex < quizQuestions.length) {
+        presentQuestion();
+    } else {
+        endQuiz();
+    }
+}
+
+function incrementScore() {
+    const scoreElement = document.getElementById("score");
+    let currentScore = parseInt(scoreElement.textContent);
+    currentScore++;
+    scoreElement.textContent = currentScore;
+}
+
+function endQuiz() {
+    clearInterval(timerInterval);
 }
 
 const quizQuestions = [
@@ -48,3 +106,42 @@ const quizQuestions = [
         answer: "3"
     },
 ];
+
+function saveScore() {
+    const initialsInput = document.getElementById("initials");
+    const initials = initialsInput.value.trim();
+
+    if (initials !== "") {
+        const score = document.getElementById("score").textContent;
+        const scoreEntry = {
+            initials: initials,
+            score: score
+        };
+
+        saveScoreToLocalStorage(scoreEntry);
+        initialsInput.value = "";
+        displayHighScores();
+    }
+}
+
+function saveScoreToLocalStorage(scoreEntry) {
+    let highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+    highScores.push(scoreEntry);
+    highScores.sort((a,b) => b.score - a.score);
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+}
+
+function displayHighScores() {
+    const highScoresList = document.getElementById("high-scores-list");
+    const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+    
+    highScores.forEach(scoreEntry => {
+        const listItem = document.createElement("li");
+        listItem.textContent = `${scoreEntry.initials}: ${scoreEntry.score}`;
+        highScoresList.appendChild(listItem);
+    });
+        
+}
+
+
+
